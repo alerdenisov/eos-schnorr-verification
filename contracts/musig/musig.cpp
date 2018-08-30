9f3aa937540b01002daaaead9d8604261a806280
Aler Denisov <aler.zampillo@gmail.com>
2018-08-30 17:53:02 +0300

    start of implementation

---
+++ contracts/musig/musig.cpp
@@ -0,0 +1,19 @@
+#include <eosiolib/eosio.hpp>
+#include "../bigint/BigIntegerLibrary.hpp"
+
+using namespace eosio;
+
+class musig : public eosio::contract
+{
+public:
+  using contract::contract;
+
+  /// @abi action
+  void exec()
+  {
+    BigInteger a = 65536;
+    print("Too much: ", (a * a * a * a * a * a * a * a));
+  }
+};
+
+EOSIO_ABI(musig, (exec))
