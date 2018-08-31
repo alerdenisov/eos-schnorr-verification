6c69df0e1fbea255c05e37352e7ff2b5e97bab6d
Aler Denisov <aler.zampillo@gmail.com>
2018-08-31 16:14:09 +0300

    allows to compile, but doesnt work :D

---
+++ contracts/musig/bigint/eos_mem_wrapper.cpp
@@ -0,0 +1,27 @@
+/** 
+ * BigInteger for EOS.IO smart contract by Mithrilcoin.io .
+ */
+#include <eosiolib/memory.hpp>
+
+using namespace eosio;
+
+// new/delete operator overloading for eos smart contract by mithrilcoin.io
+void *operator new(unsigned int size)
+{
+    return malloc(size);
+}
+
+void *operator new[](unsigned int size)
+{
+    return malloc(size);
+}
+
+void operator delete(void *ptr)
+{
+    free(ptr);
+}
+
+void operator delete[](void *ptr)
+{
+    free(ptr);
+}
